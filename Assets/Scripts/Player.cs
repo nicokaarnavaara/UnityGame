3c5a172844d1327c8e22bd138af56a2f5ec8af94
Nico Kaarnavaara <nico.kaarnavaara@tutors.es>
2026-08-25 10:43:30 +0300

    Save my local changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -122,7 +122,7 @@ public class Player : MonoBehaviour
         bool grounded = Physics.Raycast(
             feetCollider.bounds.center,
             Vector3.down,
-            0.2f,
+            0.3f,
             LayerMask.GetMask("Ground")
         );
 
