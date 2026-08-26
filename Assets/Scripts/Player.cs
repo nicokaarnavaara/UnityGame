2028413666e466ace46c6c5b9c3ee884cba0e100
Nico Kaarnavaara <nico.kaarnavaara@tutors.es>
2026-08-26 11:00:30 +0300

    fixed gravity, added post processing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,10 +12,8 @@ public class Player : MonoBehaviour
     [SerializeField] int maxJumps = 2;
     [SerializeField] BoxCollider feetCollider;
 
-    public float gravityModifier = 5f;
-
-
-    int jumpCount;
+    [Header("Gravity")]
+    [SerializeField] float gravityMultiplier = 2f;
 
     [Header("Dash")]
     [SerializeField] float dashForce = 10f;
@@ -24,6 +22,7 @@ public class Player : MonoBehaviour
 
     bool isDashing;
     bool canDash = true;
+    int jumpCount;
 
     Vector2 moveInput;
     Rigidbody rigidBody;
@@ -31,42 +30,25 @@ public class Player : MonoBehaviour
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-
-        Debug.Log("PLAYER STARTED");
-
-        if (rigidBody == null)
-        {
-            Debug.LogError("ERROR: No Rigidbody found on Player!");
-        }
-        else
-        {
-            Debug.Log("Rigidbody found!");
-        }
-
-        Physics.gravity *= gravityModifier;
     }
 
     void FixedUpdate()
     {
         Move();
+        ApplyGravity();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
 
-        Debug.Log("OnMove called! Input: " + moveInput);
-
-        // Turn the player based on movement direction
         if (moveInput.x > 0)
         {
             rigidBody.rotation = Quaternion.Euler(0f, 0f, 0f);
-            Debug.Log("Moving RIGHT");
         }
         else if (moveInput.x < 0)
         {
             rigidBody.rotation = Quaternion.Euler(0f, 180f, 0f);
-            Debug.Log("Moving LEFT");
         }
     }
 
@@ -74,7 +56,6 @@ public class Player : MonoBehaviour
     {
         if (isDashing)
         {
-            Debug.Log("Currently dashing - movement disabled");
             return;
         }
 
@@ -83,17 +64,21 @@ public class Player : MonoBehaviour
             rigidBody.linearVelocity.y,
             rigidBody.linearVelocity.z
         );
+    }
 
-        Debug.Log(
-            "Move() | Input X: " + moveInput.x +
-            " | Velocity: " + rigidBody.linearVelocity
-        );
+    void ApplyGravity()
+    {
+        if (gravityMultiplier == 1f)
+        {
+            return;
+        }
+
+        Vector3 extraGravity = Physics.gravity * (gravityMultiplier - 1f);
+        rigidBody.AddForce(extraGravity, ForceMode.Acceleration);
     }
 
     void OnJump(InputValue value)
     {
-        Debug.Log("OnJump called!");
-
         if (!value.isPressed)
         {
             return;
@@ -102,12 +87,10 @@ public class Player : MonoBehaviour
         if (IsGrounded())
         {
             jumpCount = 0;
-            Debug.Log("Grounded - jump count reset");
         }
 
         if (jumpCount >= maxJumps)
         {
-            Debug.Log("Maximum jumps reached");
             return;
         }
 
@@ -118,28 +101,20 @@ public class Player : MonoBehaviour
         );
 
         jumpCount++;
-
-        Debug.Log("JUMP! Jump count: " + jumpCount);
     }
 
     bool IsGrounded()
     {
-        bool grounded = Physics.Raycast(
+        return Physics.Raycast(
             feetCollider.bounds.center,
             Vector3.down,
             feetCollider.bounds.extents.y + 0.3f,
             LayerMask.GetMask("Ground")
         );
-
-        Debug.Log("Is Grounded: " + grounded);
-
-        return grounded;
     }
 
     void OnDash(InputValue value)
     {
-        Debug.Log("OnDash called!");
-
         if (!value.isPressed || isDashing || !canDash)
         {
             return;
@@ -153,8 +128,6 @@ public class Player : MonoBehaviour
         isDashing = true;
         canDash = false;
 
-        Debug.Log("DASH STARTED");
-
         rigidBody.AddForce(
             transform.right * dashForce,
             ForceMode.Impulse
@@ -164,12 +137,8 @@ public class Player : MonoBehaviour
 
         isDashing = false;
 
-        Debug.Log("DASH ENDED");
-
         yield return new WaitForSeconds(dashCoolDown);
 
         canDash = true;
-
-        Debug.Log("DASH READY");
     }
 }
