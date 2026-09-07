f73bf5cbb53af5268169854fb8e994eab6d54e25
Nico Kaarnavaara <nico.kaarnavaara@tutors.es>
2026-09-07 13:03:15 +0300

    Add Nico game changes with Git LFS

---
+++ Assets/Scripts/MainMenuUI_Nico.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+[DefaultExecutionOrder(1000)]
+public class MainMenuUI_Nico : MonoBehaviour
+{
+
+   
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+}
+
